ffafd240cb28f498e40209e90974de6722123ee3
Brian Manning <btreelmanning@gmail.com>
2020-11-22 21:57:09 -0500

    add percentage width tracking

--- gallery.js
+++ gallery.js
@@ -1,8 +1,9 @@
-let modalContainer, imageContainer, rows, scrim;
+let modalContainer, imageContainer, images, rows, scrim;
 window.addEventListener("DOMContentLoaded", () => {
 	rows = document.querySelectorAll(".row");
 	waitForImagesToLoad().then(setRowHeights);
 	window.addEventListener("resize", setRowHeights);
+	images = document.querySelector(".images");
 	modalContainer = document.querySelector(".modal");
 	imageContainer = document.querySelector(".modal-image");
 	scrim = document.querySelector(".scrim");
@@ -10,7 +11,6 @@ window.addEventListener("DOMContentLoaded", () => {
 		el.addEventListener("click", showImageModal(el))
 	);
 	Array.from(document.querySelectorAll("video")).forEach((el) => {
-		console.log(el);
 		el.addEventListener("click", showVideoModal(el));
 	}
 	);
@@ -18,17 +18,23 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 function calculateAR(item) {
-  const styleWidth = parseInt(item.style.width, 10);
-  const styleHeight = parseInt(item.style.height, 10);
+	let styleWidth, styleHeight, widthScale;
+	if (item.style.width.includes("%")) {
+		styleWidth = images.clientWidth * parseFloat(item.style.width) / 100;
+	} else {
+		styleWidth = parseInt(item.style.width, 10);
+	}
   const stackRows = item.querySelectorAll(".stack-row");
   if (stackRows.length > 0) {
-    console.log('ROWS', styleHeight / styleWidth);
-    return styleHeight / styleWidth;
+		return item.dataset.baseHeight / item.dataset.baseWidth;
   };
 	const imgs = item.querySelectorAll("img");
 	if (imgs.length > 1) {
 		const totalHeight = Array.from(imgs).reduce(
-			(total, img) => total + img.naturalHeight,
+			(total, img) => {
+				return total + img.naturalHeight * styleWidth / img.naturalWidth;
+			}
+			,
 			0
 		);
 		return totalHeight / styleWidth;
@@ -39,19 +45,20 @@ function calculateAR(item) {
 function calculateHeight(row) {
 	let a, b, c, d;
 	const ars = Array.from(row.children).map((item) => calculateAR(item));
+	const width = images.clientWidth;
 	switch (ars.length) {
 		case 1:
 			return null;
 		case 2:
 			[a, b] = ars;
-			return Math.floor((1180 * a * b) / (a + b));
+			return Math.floor(((width - 20) * a * b) / (a + b));
 		case 3:
 			[a, b, c] = ars;
-			return Math.floor((1160 * a * b * c) / (a * b + b * c + c * a));
+			return Math.floor(((width - 40) * a * b * c) / (a * b + b * c + c * a));
 		case 4:
 			[a, b, c, d] = ars;
 			return Math.floor(
-				(1140 * a * b * c * d) / (a * b * c + a * b * d + a * c * d + b * c * d)
+				((width - 60) * a * b * c * d) / (a * b * c + a * b * d + a * c * d + b * c * d)
 			);
 		default:
 			return null;
@@ -79,10 +86,7 @@ function hideImageModal() {
 
 function showImageModal(el) {
 	return function () {
-		imageContainer.innerHTML = `<img src='${el.src.replace(
-			"THUMBNAIL",
-			"FULLSIZE"
-		)}'>`;
+		imageContainer.innerHTML = `<img src='${el.src}'>`;
 		modalContainer.classList.add("show");
 	};
 }
